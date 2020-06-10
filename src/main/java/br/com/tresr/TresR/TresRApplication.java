91936c0f1accb1ab7c3a78cd24bf2592aa8fd81b
Sergio Gambarra <sergio@gambarra.com.br>
2020-06-10 18:08:50 -0300

    add spring structure

---
+++ src/main/java/br/com/tresr/TresR/TresRApplication.java
@@ -0,0 +1,13 @@
+package br.com.tresr.TresR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TresRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tresr/TresR/TresRApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tresr.TresR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
